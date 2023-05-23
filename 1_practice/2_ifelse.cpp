604c5d6bbcd2b77d013e8ebc772c14a90c569e9a
Grishma Shrestha <grishmashresth4@gmail.com>
2023-05-23 23:17:46 +0545

    ifelse

---
+++ 1_practice/2_ifelse.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+    cout<<"enter a number";
+    cin>>a,b;
+
+    if(a>b){
+        cout<<"a is greater than b";
+    }
+    else{
+        cout<<"b is greater than a";
+    }
+    return 0;
+}
